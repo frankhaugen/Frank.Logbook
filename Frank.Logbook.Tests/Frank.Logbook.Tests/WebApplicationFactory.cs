5a8c5c21651cc339d19e013973e6ba98cce2562c
Frank R. Haugen <frank.haugen@gmail.com>
2025-07-15 02:44:56 +0200

    Add initial project structure with basic components and configuration

---
+++ Frank.Logbook.Tests/Frank.Logbook.Tests/WebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TUnit.Core.Interfaces;
+
+namespace Frank.Logbook.Tests;
+
+public class WebApplicationFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    public Task InitializeAsync()
+    {
+        _ = Server;
+
+        return Task.CompletedTask;
+    }
+}
